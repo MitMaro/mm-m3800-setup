9e1ca20390b548c434e9f994977ead4d42bb39de
Tim Oram <mitmaro@gmail.com>
2015-05-10 20:14:37 -0230

    Added panel launcher config

---
+++ src/cookbooks/user/recipes/cinnamon.rb
@@ -0,0 +1,4 @@
+template "/home/mitmaro/.cinnamon/configs/panel-launchers@cinnamon.org/2.json" do
+	source "cinnamon/configs/panel-launchers@cinnamon.org/2.json"
+	action :create
+end
--- src/cookbooks/user/recipes/default.rb
+++ src/cookbooks/user/recipes/default.rb
@@ -1,1 +1,2 @@
 include_recipe "user::git"
+include_recipe "user::cinnamon"
