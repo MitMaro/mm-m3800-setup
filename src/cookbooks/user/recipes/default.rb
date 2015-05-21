4cf13d22c221309f14e20eab55c8c29a5276b245
Tim Oram <mitmaro@gmail.com>
2015-05-21 20:17:38 -0230

    Added inputrc file

--- src/cookbooks/user/recipes/default.rb
+++ src/cookbooks/user/recipes/default.rb
@@ -1,2 +1,3 @@
 include_recipe "user::git"
 include_recipe "user::cinnamon"
+include_recipe "user::dotfiles"
---
+++ src/cookbooks/user/recipes/dotfiles.rb
@@ -0,0 +1,4 @@
+template "/home/mitmaro/.inputrc" do
+	source "dotfiles/inputrc.erb"
+	action :create
+end
