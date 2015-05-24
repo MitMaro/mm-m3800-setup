460729b7e7316a4e04495a61a9cb98e382598c45
Tim Oram <mitmaro@gmail.com>
2015-05-24 01:06:43 -0230

    Added initial bashrc

--- src/cookbooks/user/recipes/dotfiles.rb
+++ src/cookbooks/user/recipes/dotfiles.rb
@@ -2,3 +2,27 @@ template "/home/mitmaro/.inputrc" do
 	source "dotfiles/inputrc.erb"
 	action :create
 end
+
+template "/home/mitmaro/.bashrc" do
+	source "dotfiles/bashrc.erb"
+	action :create
+end
+
+directory "/home/mitmaro/.bashrc.d/" do
+	recursive true
+end
+
+template "/home/mitmaro/.bashrc.d/010-functions" do
+	source "dotfiles/bashrc.d/functions.erb"
+	action :create
+end
+
+template "/home/mitmaro/.bashrc.d/100-rvm" do
+	source "dotfiles/bashrc.d/rvm.erb"
+	action :create
+end
+
+template "/home/mitmaro/.bashrc.d/500-bash-settings" do
+	source "dotfiles/bashrc.d/bash-settings.erb"
+	action :create
+end
