4ae45d56ae8e149ca1fd7d23aea8c1472c84adc4
Tim Oram <mitmaro@gmail.com>
2015-05-30 16:40:22 -0230

    Installed virtual box

---
+++ src/cookbooks/system/recipes/virtualbox.rb
@@ -0,0 +1,10 @@
+apt_repository 'virtualbox' do
+	uri           'http://download.virtualbox.org/virtualbox/debian'
+	distribution  node['lsb']['codename']
+	components    ['contrib']
+	key           'http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc'
+end
+
+package "virtualbox\-4.3" do
+	action :install
+end
