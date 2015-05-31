8dc78618df6e083e72f9efbf443ffbfb6660ae7d
Tim Oram <mitmaro@gmail.com>
2015-05-31 13:04:28 -0230

    Use ubuntu key server

--- setup.sh
+++ setup.sh
@@ -23,7 +23,7 @@ fi
 
  if [ ! -e ~/.rvm/scripts/rvm ]; then
 	echo "rvm not installed, installing"
-	gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+	gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 	curl -sSL https://get.rvm.io | bash -s stable --ruby
  fi
 
