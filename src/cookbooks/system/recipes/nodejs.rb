b0e9e7aa9d22cbf939dcafc09c417a2985213726
Tim Oram <mitmaro@gmail.com>
2015-05-31 18:14:37 -0230

    Install system nodejs

---
+++ src/cookbooks/system/recipes/nodejs.rb
@@ -0,0 +1,10 @@
+apt_repository 'nodejs' do
+	uri           'https://deb.nodesource.com/node_0.10'
+	distribution  node['lsb']['codename']
+	components    ['main']
+	key           'https://deb.nodesource.com/gpgkey/nodesource.gpg.key'
+end
+
+package "nodejs" do
+	action :install
+end
